90e8609ca774a6e81df3f2e14a6907624cb43fe4
arraaashu <arra.aashritha@gmail.com>
2024-09-19 20:14:10 -0500

    Successfully built the project of implementing the functionality of converting the typescript type to Objects

---
+++ src/index.ts
@@ -0,0 +1,54 @@
+import * as ts from "typescript";
+
+// This function is for traversing through the AST nodes.
+const visit = (node: ts.Node, obj: any): void => {
+  // Checking if the node is a type declaration
+  if (ts.isTypeAliasDeclaration(node)) {
+    const name = node.name.getText();
+    obj[name.toLowerCase()] = {};
+    const typeNode = node.type;
+
+    // If the type is an object typewe will be handling its properties
+    if (ts.isTypeLiteralNode(typeNode)) {
+      typeNode.members.forEach((member) => {
+        if (ts.isPropertySignature(member)) {
+          const propName = (member.name as ts.Identifier).getText();
+          const propType = member.type;
+
+          if (propType && ts.isUnionTypeNode(propType)) {
+            // If the property type is a combination of strings then  extracting the options
+            const values = propType.types
+              .filter(ts.isLiteralTypeNode)
+              .map((literalType) => (literalType.literal as ts.StringLiteral).text);
+            obj[name.toLowerCase()][`${propName}s`] = values;
+          }
+        }
+      });
+    }
+  }
+
+  // Recursively visiting the child nodes
+  ts.forEachChild(node, (child) => visit(child, obj));
+};
+
+// convertToObject function is parsing the input type string and calling the visit function
+const convertToObject = (type: string): any => {
+  const sourceFile = ts.createSourceFile(
+    "temp.ts", // temp file
+    type,      // input: TypeScript code
+    ts.ScriptTarget.Latest,
+    true,      // for parsing in a script mode
+    ts.ScriptKind.TS
+  );
+
+  const result: any = {};
+  visit(sourceFile, result);
+  return result;
+};
+
+//test case
+const typeString = `type Button = {
+  variant: "solid" | "text";
+};`;
+
+console.log(convertToObject(typeString));
